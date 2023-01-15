576284cdf2f6f28c9f00c1313f1912ffc5717a62
Puspendu sanki <122733897+puspendu2003@users.noreply.github.com>
2023-01-15 20:14:56 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,16 @@
+import java.util . *;
+public class p1
+ {
+    public static void main(String args[])
+    {
+        System.out.println("Enter two input for addition");
+        //INPUT
+        Scanner ob=new Scanner(System.in);
+        int a=ob.nextInt();
+        int b=ob.nextInt();
+        int sum=a+b;
+        System.out.println("Addition of the two number is:"+sum);
+    }
+
+    
+}
